9394516b094ca8a07ea1d430f2ec6cf6760b8311
mayerxc <mayerxc@gmail.com>
2022-12-26 15:25:53 -0500

    WIP

--- main.py
+++ main.py
@@ -10,16 +10,32 @@ def index():
     return render_template("index.html")
 
 
-# @app.route("/download/<youtube_link>")
-# def download(youtube_link):
 @app.route("/download/")
 def download():
     youtube_link = request.args.get("youtube")
     print("youtube link", youtube_link)
-    # return {"youtubeLink": youtube_link}
     buffer = BytesIO()
     video = (
         YouTube(youtube_link).streams.filter(file_extension="mp4", res="720p").first()
+    )
+    title = video.title
+    video.stream_to_buffer(buffer)
+    buffer.seek(0)
+    return send_file(
+        buffer,
+        as_attachment=True,
+        download_name=f"{title}.mp4",
+        mimetype="video/mp4",
+    )
+
+
+
+@app.route("/download2/<path:youtube_link2>")
+def download2(youtube_link2):
+    print("youtube link is:", youtube_link2)
+    buffer = BytesIO()
+    video = (
+        YouTube(youtube_link2).streams.filter(file_extension="mp4", res="720p").first()
     )
     title = video.title
     video.stream_to_buffer(buffer)
